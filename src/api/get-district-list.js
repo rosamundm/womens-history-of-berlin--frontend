fab8a10d3e791c78cccd98758cb76002ff1237c8
Rosamund Mather <rsmnd.mthr@gmail.com>
2023-02-14 21:20:17 +0100

    Add lambda function get-district-list

---
+++ src/api/get-district-list.js
@@ -0,0 +1,32 @@
+const axios = require("axios");
+require("dotenv").config();
+
+exports.handler = async (event, context) => {
+
+  let response
+
+  try {
+    response = await axios.get(`${process.env.REACT_APP_PROD_API_URL}/districts`, {
+        headers: {
+            "Authorization": `JWT ${process.env.REACT_APP_PROD_JWT_TOKEN}`,
+            "Accept": "application/json", 
+            "Content-Type": "application/json"
+        }
+    })
+
+  } catch (err) {
+    return {
+      statusCode: err.statusCode || 500,
+      body: JSON.stringify({
+        error: err.message
+      })
+    }
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      data: response.data
+    })
+  }
+}
