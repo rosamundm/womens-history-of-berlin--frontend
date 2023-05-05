8fd0ee8ece072f355ca9d69d23211b9bdda0614b
Rosamund Mather <rsmnd.mthr@gmail.com>
2023-05-05 14:26:36 +0200

    Change proxy path for production

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,8 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function(app) {
   app.use(
     createProxyMiddleware("/.netlify/functions/", {
-      target: "http://localhost:9000/",
+      target: "https://lustrous-pavlova-95371e.netlify.app/",
+      // target: "http://localhost:9000/",
       pathRewrite: {
         "^/\\.netlify/functions": "",
       },
