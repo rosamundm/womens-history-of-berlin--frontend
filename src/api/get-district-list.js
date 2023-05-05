5e7a8b0124382c60fb1573c17b89418b8120527f
Rosamund Mather <rsmnd.mthr@gmail.com>
2023-05-05 19:19:05 +0200

    Rewrite one function to return axios fetch

--- src/api/get-district-list.js
+++ src/api/get-district-list.js
@@ -1,42 +1,63 @@
 const axios = require("axios");
 require("dotenv").config();
 
-exports.handler = async (context) => {
+exports.handler = async (event, context) => {
 
-  let response
-
-  try {
-    response = await axios.get(`${process.env.REACT_APP_PROD_API_URL}/districts`, {
+    return axios({
+        method: "get",
+        url: `${process.env.REACT_APP_PROD_API_URL}/districts`,
         headers: {
-            "Authorization": `JWT ${process.env.REACT_APP_PROD_JWT_TOKEN}`,
-            "Accept": "application/json", 
-            "Content-Type": "application/json"
+          "Authorization": `JWT ${process.env.REACT_APP_PROD_JWT_TOKEN}`,
+          "Accept": "application/json", 
+          "Content-Type": "application/json"
         }
-    })
+      })
+        .then((response) => ({
+            statusCode: 200,
+            body: JSON.stringify(response.data),
+        }))
+        .catch((error) => ({
+            statusCode: 500,
+            body: JSON.stringify(error.message),
+        }));
 
-  } catch (err) {
-    return {
-      statusCode: err.statusCode,
-      body: JSON.stringify({
-        error: err.message
-      }),
-      // function headers need to be specified separately 
-      // from rest of code!
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Content-Type": "application/json"
-      },
-    }
-  }
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      data: response.data
-    }),
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Content-Type": "application/json"
-    },
-  }
+
+  // let response
+
+  // try {
+  //   response = await axios.get(`${process.env.REACT_APP_PROD_API_URL}/districts`, {
+  //       headers: {
+  //           "Authorization": `JWT ${process.env.REACT_APP_PROD_JWT_TOKEN}`,
+  //           "Accept": "application/json", 
+  //           "Content-Type": "application/json"
+  //       }
+  //   })
+
+  // } catch (err) {
+  //   return {
+  //     statusCode: err.statusCode,
+  //     body: JSON.stringify({
+  //       error: err.message
+  //     }),
+  //     // function headers need to be specified separately 
+  //     // from rest of code!
+  //     headers: {
+  //       "Access-Control-Allow-Origin": "*",
+  //       "Content-Type": "application/json"
+  //     },
+  //   }
+  // }
+
+  // return {
+  //   statusCode: 200,
+  //   body: JSON.stringify({
+  //     data: response.data
+  //   }),
+  //   headers: {
+  //     "Access-Control-Allow-Origin": "*",
+  //     "Content-Type": "application/json"
+  //   },
+  // }
 }
+
