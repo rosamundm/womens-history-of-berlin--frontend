05e8d36ce2e9399a72a908d3e26ddc49a9fb2369
Rosamund Mather <rsmnd.mthr@gmail.com>
2023-05-14 17:11:56 +0200

    Test title text wrap

--- src/components/District.js
+++ src/components/District.js
@@ -48,13 +48,15 @@ export default function DistrictInstance() {
 
             <div class="container p-8 bg-slate-100">
 
-                <div class="p-6 text-6xl">{districtInstance.data.name}</div>
+                <div class="p-6 text-4xl">
+                    {districtInstance.data.name}
+                </div>
              
                 <div className="street-list" class="p-8">
                     {districtInstance.data.streets.map((street) => (
 
                         <div 
-                        className="text-3xl p-3"
+                        className="text-2xl p-3"
                             key={street.name} onClick={() => setSelectedStreet(street)}>
                             <Link to={`/streets/${street.street_slug}/`}>
                                 {street.name}
@@ -82,7 +84,9 @@ export default function DistrictInstance() {
         
             <div class="container p-8 bg-slate-100">
         
-                <div class="p-6 text-6xl">{districtInstance.data.name}</div>
+                <div class="p-6 text-4xl">
+                    {districtInstance.data.name}
+                </div>
                      
                     <div className="street-list" class="p-8">
                         No streets added yet for this district — keep checking back!
