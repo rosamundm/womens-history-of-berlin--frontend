7b221ca589c0a3e6e1a6c47cc35f55bdfdca04b5
Rosamund Mather <rsmnd.mthr@gmail.com>
2023-05-15 20:41:52 +0200

    Change homepage title

--- src/components/layout/HomepageHeader.js
+++ src/components/layout/HomepageHeader.js
@@ -4,9 +4,9 @@ function HomepageHeader() {
     return (
        <div class="p-5 py-10 bg-violet-300 rounded-lg shadow">
         <div class="font-serif text-6xl text-center">
-            Berliner Frauen:
+            women's history of berlin:
             <div class="text-4xl py-10">
-                a project dedicated to women's legacy on Berlin's streets
+                a project dedicated to the legacy of women in the city's streets
                 <br></br>
                 <span class="text-6xl">
                     <Link to={"/districts/"}>
