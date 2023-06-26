c80216286eb143641438971f941cd5a0ba245a5f
Rosamund Mather <rsmnd.mthr@gmail.com>
2023-06-26 21:26:34 +0200

    Change site name

--- src/components/layout/HomepageHeader.js
+++ src/components/layout/HomepageHeader.js
@@ -4,9 +4,9 @@ function HomepageHeader() {
     return (
        <div class="p-5 py-10 bg-violet-300 rounded-lg shadow">
         <div class="font-serif font-semibold text-6xl text-center">
-            women's history of berlin
+            named after women
             <div class="text-4xl font-light py-10">
-                a project dedicated to the legacy of women in city street names
+                a project dedicated to the legacy of women in berlin's street names
                 <br></br>
                 <span class="text-6xl">
                     <Link to={"/districts/"}>
