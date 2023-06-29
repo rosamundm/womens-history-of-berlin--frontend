1f8df554e3327b41a55d7e09b5e1da406878e6b2
Rosamund Mather <rsmnd.mthr@gmail.com>
2023-06-29 21:18:33 +0200

    Add blog/news pages

---
+++ src/api/get-blog-post-instance.js
@@ -0,0 +1,41 @@
+const axios = require("axios");
+require("dotenv").config();
+
+exports.handler = async (event) => {
+
+  const { slug } = event.queryStringParameters;
+  let response
+
+  try {
+    response = await axios.get(`${process.env.REACT_APP_PROD_API_URL}/blog/${slug}`, {
+        headers: {
+            "Authorization": `JWT ${process.env.REACT_APP_PROD_JWT_TOKEN}`,
+            "Accept": "application/json", 
+            "Content-Type": "application/json"
+        }
+    })
+
+  } catch (err) {
+    return {
+      statusCode: err.statusCode,
+      body: JSON.stringify({
+        error: err.message
+      }),
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json"
+      },
+    }
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      data: response.data  // access at /.netlify/functions/get-blog-post-instance?slug={slug}
+    }),
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Content-Type": "application/json"
+    },
+  }
+}
---
+++ src/components/BlogPost.js
@@ -0,0 +1,81 @@
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import parse from 'html-react-parser';
+import Footer from "./layout/Footer";
+
+export default function BlogPostInstance() {
+
+    let { slug } = useParams();
+    const [postInstance, setPostInstance] = useState(null);
+
+    useEffect(() => {
+
+      if (!slug) {
+          return;
+      }
+
+      (async () => {
+          const response = await fetch(
+            `/.netlify/functions/get-blog-post-instance?slug=${slug}`,
+            {method: "GET"}
+          ).then((response) => response.json());
+            setPostInstance(response);
+      })();
+
+    }, [slug]);
+
+    if (!postInstance) {
+      return (
+        <div class="container p-8 bg-slate-100">
+            <div class="p-6 text-3xl">
+                Loading post...
+                <div class="py-3 text-xl">
+                  If it takes too long, please try again or check that the URL is valid.
+                </div>
+            </div>
+            <div>
+                <Footer/>
+            </div>
+        </div>
+      );
+    }
+
+    const parsedPostBody = parse(postInstance.data.body)
+
+    return (
+
+        <div class="container p-8 bg-slate-100">
+
+          <div className="post-detail" class="p-6 bg-sky-100">
+
+              <div class="sm:p-2.5 md:p-5 lg:p-5 bg-sky-100">
+
+                <div class="text-4xl">
+                    {postInstance.data.title}
+                </div>
+               
+                <div class="text-base p-5">
+                    {postInstance.data.published} 
+                </div>
+
+                <div class="p-4 sm:text-lg md:text-2xl lg:text-2xl sm:text-left md:text-justify lg:text-justify">
+                  {parsedPostBody}
+                </div>
+
+              </div>
+   
+          </div>
+            
+          <div class="p-6">
+            <Link to={"/news/"}>
+                Back to blog
+            </Link>
+          </div>
+
+          <div>
+            <Footer/>
+          </div>
+
+        </div>
+    );
+};
