060520194c67c880171377596b7f88d2bfa8fa64
Rosamund Mather <rsmnd.mthr@gmail.com>
2023-08-10 16:26:59 +0200

    Add button and link for map

--- src/components/layout/HomepageHeader.js
+++ src/components/layout/HomepageHeader.js
@@ -2,19 +2,25 @@ import { Link } from "react-router-dom";
 
 function HomepageHeader() {
     return (
-       <div class="p-5 py-10 bg-violet-300 rounded-lg shadow">
-        <div class="font-serif font-semibold text-6xl text-center">
+       <div class="p-5 py-10 bg-violet-300 rounded-lg shadow text-center">
+        <div class="font-serif font-semibold text-6xl pb-4">
             named after women
-            <div class="text-4xl font-light py-10">
-                a project dedicated to the legacy of women in berlin's street names
-                <br></br>
-                <span class="text-6xl">
-                    <Link to={"/districts/"}>
-                    ⟶
-                    </Link>
-                </span>
-            </div>
         </div>
+            <div class="text-4xl font-light pb-4 py-10">
+                a project dedicated to the legacy of women in berlin's street names
+            </div>
+            <div class="py-5">
+                <Link to={"/districts/"} class="md:px-4">
+                    <button class="bg-indigo-800 hover:bg-violet-300 text-white text-xl px-4 py-2 rounded">
+                        grid view
+                    </button>
+                </Link>
+                <Link to={"/map/"} class="md:px-4">
+                    <button class="bg-indigo-800 hover:bg-violet-300 text-white text-xl px-4 py-2 rounded">
+                        map view
+                    </button>
+                </Link>
+            </div>
        </div>
     )
 };
