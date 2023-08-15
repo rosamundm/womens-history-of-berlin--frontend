57fdda22c8115e2daba007352e7404af4774598b
Rosamund Mather <rsmnd.mthr@gmail.com>
2023-08-15 16:10:25 +0200

    Expand Map page

--- src/components/Map.js
+++ src/components/Map.js
@@ -53,33 +53,54 @@ export default function Map() {
 
         return (
 
-            <>
-            <div className="landing" class="px-500 justify-center justify-items-center max-w-4xl m-auto py-8">
-                <MapContainer
-                    center={[52.5170124, 13.389094]}
-                    zoom={10}
-                    style={{ height: "100vh", width: "100vh" }}
-                >
-                <getMapCenter />
-                <TileLayer
-                    attribution={attribution}
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
+            <div class="container p-8 py-1 bg-slate-100">
 
-                    {mapStreets.data.map(mapStreet => (
-                        <Marker position={mapStreet.geocode} icon={customIcon}>
-                            <Popup>
-                                <Link
-                                    target="_blank"
-                                    to={`/streets/${mapStreet.street_slug}`}>
-                                        {mapStreet.name}
-                                </Link>
-                            </Popup>
-                        </Marker>
-                    ))}
-                </MapContainer>
+                <div class="p-6 bg-sky-100">
+                    
+                    <div class="p-5 px-2.5">
+                        <div class="text-4xl py-4">
+                            Map
+                        </div>
+                        <p class="pb-2.5 text-xl">
+                            Here's a topographic representation of the Berlin streets named after women. Each marker contains a link that leads to the page telling
+                            the story of the street's eponym (opens in a new tab).
+                            </p>
+                        <p class="pb-2.5 text-xl">
+                            <b>NB:</b> These are only the streets whose entries are complete. As explained <a href="http://localhost:3000/about">here</a>, 
+                            the website is being continuously updated!
+                        </p>
+                        <div className="landing" class="px-500 justify-center justify-items-cewnter max-w-4xl m-auto py-8 sm:py-14 md:py-14">
+                            <MapContainer
+                                center={[52.5170124, 13.389094]}
+                                zoom={10}
+                                style={{ height: "100vh", width: "100%" }}
+                            >
+                            <getMapCenter />
+                            <TileLayer
+                                attribution={attribution}
+                                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                            />
+
+                                {mapStreets.data.map(mapStreet => (
+                                    <Marker position={mapStreet.geocode} icon={customIcon}>
+                                        <Popup>
+                                            <Link
+                                                target="_blank"
+                                                to={`/streets/${mapStreet.street_slug}`}>
+                                                    {mapStreet.name}
+                                            </Link>
+                                        </Popup>
+                                    </Marker>
+                                ))}
+                            </MapContainer>
+                        </div>
+                    </div>
+
+                    <Footer/>
+
+                </div>
+
             </div>
-        </>
 
         )
     }
