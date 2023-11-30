d37bc48e600a28aa9e9fd249172be6c42956a5e4
Rosamund Mather <rsmnd.mthr@gmail.com>
2023-11-30 19:25:39 +0100

    Add get-tag-instance and get-tag-list

---
+++ src/api/get-tag-instance.js
@@ -0,0 +1,42 @@
+const axios = require("axios");
+require("dotenv").config();
+
+exports.handler = async (event) => {
+
+  const { slug } = event.queryStringParameters;
+
+  let response
+
+  try {
+    response = await axios.get(`${process.env.REACT_APP_PROD_API_URL}/tags/${slug}`, {
+        headers: {
+            "Authorization": `JWT ${process.env.REACT_APP_PROD_JWT_TOKEN}`,
+            "Accept": "application/json", 
+            "Content-Type": "application/json"
+        }
+    })
+
+  } catch (err) {
+    return {
+      statusCode: err.statusCode,
+      body: JSON.stringify({
+        error: err.message
+      }),
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json"
+      },
+    }
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      data: response.data  // access at /.netlify/functions/get-tag-instance?slug={slug}
+    }),
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Content-Type": "application/json"
+    },
+  }
+}
