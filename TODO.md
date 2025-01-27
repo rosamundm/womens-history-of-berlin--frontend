553fa29db68adacd3de17d4784306dbcc948d905
Rosamund Mather <rsmnd.mthr@gmail.com>
2025-01-27 19:53:10 +0100

    Migrate env variables

--- TODO.md
+++
@@ -1,4 +0,0 @@
-- set up `process.env` config
-- add cookie settings pop-up
-- add photo grid for street list view
-- add favicon
--- src/api/get-page-instance.js
+++ src/api/get-page-instance.js
@@ -7,10 +7,10 @@ exports.handler = async (event) => {
 
   try {
     response = await axios.get(
-      `${process.env.REACT_APP_PROD_API_URL}/pages/${slug}`,
+      `${import.meta.env.VITE_PROD_API_URL}/pages/${slug}`,
       {
         headers: {
-          Authorization: `JWT ${process.env.REACT_APP_PROD_JWT_TOKEN}`,
+          Authorization: `JWT ${import.meta.env.VITE_PROD_JWT_TOKEN}`,
           Accept: "application/json",
           "Content-Type": "application/json",
         },
